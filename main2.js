ecafffda8e9c547309e18f558d3968e43ca8a8d2
BankVana <32451295+BankVana@users.noreply.github.com>
2017-10-03 18:30:52 +0700

    Revert "firefox android check(3)"
    This reverts commit 96b39549f94baf2bc75e746befe3dd88febc1d96.

--- main2.js
+++ main2.js
@@ -20,11 +20,7 @@ var constraints_list = ['environment','user'];
 				 || navigator.userAgent.match(/iPod/i)
 				 || navigator.userAgent.match(/BlackBerry/i)
 				 || navigator.userAgent.match(/Windows Phone/i)
-<<<<<<< HEAD
-				 ) && /Android.+Firefox\//.test(navigator.userAgent)
-=======
-				 ) && navigator.mozGetUserMedia
->>>>>>> parent of 96b3954... firefox android check(2)
+				 ) && navigator.userAgent.toLowerCase().indexOf('fennec') > -1
 				 ){
 					return true;
 				  }
